c9218109f9d225f9bed6dbf222aec137059aa2e2
victor <victordsgamorim@hotmail.com>
2019-09-17 23:31:24 +0100

    teste

--- app/src/main/java/com/victor/stockpesistentdata/StockListActivity.java
+++ app/src/main/java/com/victor/stockpesistentdata/StockListActivity.java
@@ -4,14 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.sql.SQLOutput;
+
 public class StockListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock_list);
-
-
+        System.out.println("Hello World");
 
     }
 }
