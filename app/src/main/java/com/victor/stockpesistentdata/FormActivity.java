e25064791854fbdea236cc16df4f207b73796cd8
victor <victordsgamorim@hotmail.com>
2019-09-18 00:32:03 +0100

    stock ui, as recyclerview and cardview

---
+++ app/src/main/java/com/victor/stockpesistentdata/FormActivity.java
@@ -0,0 +1,33 @@
+package com.victor.stockpesistentdata;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class FormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_done_item, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.menu_done_add_item) {
+            //TODO add item
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/victor/stockpesistentdata/StockListActivity.java
+++ app/src/main/java/com/victor/stockpesistentdata/StockListActivity.java
@@ -2,7 +2,12 @@ package com.victor.stockpesistentdata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.FloatArrayEvaluator;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.sql.SQLOutput;
 
@@ -12,6 +17,18 @@ public class StockListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock_list);
-        System.out.println();
+
+        FloatingActionButton fab = findViewById(R.id.activity_fab_add_item);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //TODO open form activity in order to add item
+                Intent intent = new Intent(StockListActivity.this, FormActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
